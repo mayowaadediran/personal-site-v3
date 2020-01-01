1005145a44ad27a37514ec7391e53624af2ba637
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2020-01-01 23:37:52 +0100

    create layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,25 +9,64 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import "./layout.css"
+import { Link } from "gatsby"
+
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 const Layout = ({ children }) => {
 
   return (
-    <>
       <div
         style={{
           margin: `0 auto`,
+          minHeight: `100vh`,
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
+          display: `flex`,
+          flexDirection: `column`,
+          justifyContent: `space-between`,
         }}
       >
-        <main>{children}</main>
-        <footer>
+        <header style={{ marginBottom: `1.5rem` }}>
+          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+            <h3 style={{ display: `inline` }}>MySweetSite</h3>
+          </Link>
+          <ul style={{ listStyle: `none`, float: `right` }}>
+            <ListLink to="/about/">about me</ListLink>
+            <ListLink to="/works/">works</ListLink>
+            <ListLink to="/writings/">writings</ListLink>
+          </ul>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer
+          style={{
+            display: `flex`,
+            justifyContent: `space-between`
+          }}>
+          <p>
           © {new Date().getFullYear()}, Mayowa Adediran 
+          </p>
+          <ul  
+            style={{
+              display: `flex`,
+              justifyContent: `space-between`,
+            }}>
+            <li style={{ display: `inline-block`, marginLeft: `1rem` }}>
+              <a href='https://github.com/mayowaadediran'>Github</a>
+            </li>
+            <li style={{ display: `inline-block`, marginLeft: `1rem` }}>
+              <a href='https://www.linkedin.com/in/mayowa-adediran/' target='blank' >LinkedIn</a>
+            </li>
+          </ul>
         </footer>
       </div>
-    </>
   )
 }
 
