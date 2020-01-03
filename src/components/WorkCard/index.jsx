bfa67e6a3efde18d93af10ec02f7880640789bc8
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2020-01-03 17:07:59 +0100

    works implementation

--- src/components/WorkCard/index.jsx
+++ src/components/WorkCard/index.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import styles from './workCard.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-const index = () => {
+const index = (props) => {
+  const works = props.works
+
   return (
-    <div>
-      
+    <div className={styles.cardContainer}>
+      {works.map((work, i) => {
+        return (
+          <div className={styles.card} key={i}>
+            <h5>{work.node.title}</h5>
+            <p>{work.node.description}</p>
+            <div className={styles.cardBottom}>
+              <ul className={styles.tags}>
+                {work.node.tag
+                  ? work.node.tag.map((tags, i) => {
+                      return <h6 key={i}>{tags}</h6>
+                    })
+                  : null}
+              </ul>
+            </div>
+            <div className={styles.link}>
+              {work.node.link ? (
+                <a href={work.node.link}>
+                  <FontAwesomeIcon icon={faExternalLinkAlt} />
+                </a>
+              ) : null}
+              {work.node.github ? (
+                <a href={work.node.github}>
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              ) : null}
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/pages/works.js
+++ src/pages/works.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from "../components/seo"
+import SEO from "../components/seo";
+import WorkCard from "../components/WorkCard"
+import {
+  graphql,
+} from 'gatsby';
 
 export const query = graphql `
   query WorksPageQuery {
-    allContentfulWorks {
-      edges {
-        node {
-          title
-          description
-          link
-          image {
-            file {
-              url
-            }
-          }
-          category
-          tag
-        }
-      }
-    }
+   allContentfulWorks {
+     edges {
+       node {
+         github
+         description
+         category
+         title
+         link
+         tag
+       }
+     }
+   }
   }
 `
 
-const Works = () => {
+const Works = ({data}) => {
+
+  const professionalWork = data.allContentfulWorks.edges.filter(item => item.node.category === "Professional")
+  const personalWork = data.allContentfulWorks.edges.filter(item => item.node.category === "Personal")
+
   return (
     <Layout>
       <SEO title="Work" />
       <div>
         <h1>works</h1>
-        <p>Work done personally or professionally as part of a team</p>
       </div>
       <div>
-        
+        <WorkCard works={professionalWork}/>
       </div>
       <div>
-
+        <h4>Personal Work</h4>
+        <WorkCard works={personalWork}/>
       </div>
     </Layout>
   );
